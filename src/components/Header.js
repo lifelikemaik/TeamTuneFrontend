90140137e94f9d039b058cba5e373f847baf9312
David Satomi <d.satomi@outlook.de>
2021-07-03 12:08:24 +0200

    Added Hamburger menu for narrow screens

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import { AppBar, Button, IconButton, Toolbar, Divider, Typography } from "@material-ui/core";
+import React, {useEffect, useState} from "react";
+import {withRouter} from "react-router-dom";
+import {makeStyles} from "@material-ui/core/styles";
+import {AppBar, Button, IconButton, Toolbar, Divider, Typography, Drawer, MenuItem, Link} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 import TeamTuneIcon from "./TeamTuneIcon";
 
-import { useSelector } from "react-redux";
-import { logout } from "../redux/actions";
+import {useSelector} from "react-redux";
+import {logout} from "../redux/actions";
 
 const useStyles = makeStyles((theme) => ({
 
@@ -29,12 +30,18 @@ const useStyles = makeStyles((theme) => ({
     header: {
         paddingRight: 80,
         paddingLeft: 110,
+        "@media (max-width: 900px)": {
+            paddingLeft: 0,
+        },
     },
     logo_type: {
         fontFamily: "Libre Franklin, sans-serif",
     },
     icon_type: {
         marginLeft: 100,
+    },
+    drawerContainer: {
+        padding: "20px 30px",
     },
 }));
 
@@ -43,13 +50,35 @@ const useStyles = makeStyles((theme) => ({
  * @param {props} props
  */
 function Header(props) {
-    const { header, main_options, side_options, toolbar, logo_type, icon_type } = useStyles();
+    const {header, main_options, side_options, toolbar, logo_type, icon_type, drawerContainer} = useStyles();
 
 
     const user = useSelector((state) => {
         // return the currently logged in user from redux store
         return state.user;
     });
+
+    const [state, setState] = useState({
+        mobileView: false,
+        drawerOpen: false,
+    });
+
+    const {mobileView, drawerOpen} = state;
+
+    useEffect(() => {
+        const setResponsiveness = () => {
+            return window.innerWidth < 900
+                ? setState((prevState) => ({...prevState, mobileView: true}))
+                : setState((prevState) => ({...prevState, mobileView: false}));
+        };
+
+        setResponsiveness();
+        window.addEventListener("resize", () => setResponsiveness());
+
+        return () => {
+            window.removeEventListener("resize", () => setResponsiveness());
+        }
+    }, []);
 
     const onClickLogout = () => {
         // trigger redux logout action
@@ -58,7 +87,7 @@ function Header(props) {
         props.history.push("/");
     };
 
-    const displayHeader = () => {
+    const displayDesktop = () => {
         return (
             <Toolbar className={toolbar}>
                 {teamTuneLogo}
@@ -69,90 +98,148 @@ function Header(props) {
         );
     };
 
+    const displayMobile = () => {
+        const handleDrawerOpen = () =>
+            setState((prevState) => ({...prevState, drawerOpen: true}));
+        const handleDrawerClose = () =>
+            setState((prevState) => ({...prevState, drawerOpen: false}));
+        return (
+            <Toolbar>
+                <IconButton
+                    {...{
+                        edge: "start",
+                        color: "inherit",
+                        onClick: handleDrawerOpen,
+                    }}
+                >
+                    <MenuIcon/>
+                </IconButton>
+                <Drawer
+                    {...{
+                        anchor: "left",
+                        open: drawerOpen,
+                        onClick: handleDrawerClose,
+                        onClose: handleDrawerClose,
+                    }}
+                >
+                    <div className={drawerContainer}>{getDrawerChoices()}</div>
+                </Drawer>
+                {/*<div>{teamTuneLogo}</div>*/}
+            </Toolbar>
+        );
+    };
+
     const logoType = (
-        <Typography variant="h4"  className={logo_type}>
+        <Typography variant="h4" className={logo_type}>
             TeamTune
         </Typography>
     );
 
     const teamTuneLogo = (
-        <IconButton onClick={() => props.history.push("/")} color="inherit" classname={icon_type}>
-            <div style={{ width: 50, marginLeft: -100 }}>
-                <TeamTuneIcon />
-            </div>
-            {logoType}
-        </IconButton>
+        <div>
+            <IconButton onClick={() => props.history.push("/")} color="inherit" classname={icon_type}>
+                <div style={{width: 50, marginLeft: -100}}>
+                    <TeamTuneIcon/>
+                </div>
+                {logoType}
+            </IconButton>
+        </div>
+
     );
 
-    const getMenuButtons = () => {
-        return <div>
-            <section className={toolbar}>
-                <Button
-                    className={main_options}
-                    color="inherit"
-                    onClick={() => props.history.push("/browse")}
-                >
-                    Browse
-                </Button>
-                {user.user
-                    ? [
-                        // Buttons to show when user is logged in
-                        <Button
-                            className={main_options}
-                            color="inherit"
-                            onClick={() => props.history.push("/playlists")}
+    const headersData = [
+        {
+            label: "Browse",
+            req_login: false,
+            req_logout: false,
+            className: main_options,
+            onClickFun: () => props.history.push("/browse")
+        },
+        {
+            label: "My Playlists",
+            req_login: true,
+            req_logout: false,
+            className: main_options,
+            onClickFun: () => props.history.push("/playlists")
+        },
+        {
+            label: "My TeamTune",
+            req_login: true,
+            req_logout: false,
+            className: main_options,
+            onClickFun: () => props.history.push("/myteamtune")
+        },
+        {
+            label: "Log out",
+            req_login: true,
+            req_logout: false,
+            className: side_options,
+            onClickFun: onClickLogout
+        },
+        {
+            label: "Log in",
+            req_login: false,
+            req_logout: true,
+            className: side_options,
+            onClickFun: () => props.history.push("/login")
+        },
+        {
+            label: "Sign up",
+            req_login: false,
+            req_logout: true,
+            className: side_options,
+            onClickFun: () => props.history.push("/register")
+        },
+
+    ];
+
+    const getDrawerChoices = () => {
+        return headersData.map(({ label, req_login, req_logout, className, onClickFun }) => {
+            return (
+                <React.Fragment>
+                    { ((!!(user.user) && req_login) || !(user.user) && req_logout || req_logout === req_login) ? [
+                        <Link
+                            {...{
+                                key: label,
+                                onClick: onClickFun,
+                                className: className,
+                            }}
                         >
-                            My Playlists
-                        </Button>,
-                        <Button
-                            className={main_options}
-                            color="inherit"
-                            onClick={() => props.history.push("/myteamtune")}
-                        >
-                            My TeamTune
-                        </Button>,
-                        <Divider orientation="vertical" flexItem variant="middle" />,
-                        <Button
-                            className={side_options}
-                            color="inherit"
-                            onClick={onClickLogout}
-                        >
-                            Log out
-                        </Button>
-                    ]
-                    : [
-                        // Buttons to show when user is not logged in
-                        <Button
-                            className={main_options}
-                            color="inherit"
-                        >
-                            Premium
-                        </Button>,
-                        <Divider orientation="vertical" flexItem variant="middle" />,
-                        <Button
-                            className={side_options}
-                            color="inherit"
-                            onClick={() => props.history.push("/login")}
-                        >
-                            Log in
-                        </Button>,
-                        <Button
-                            className={side_options}
-                            color="inherit"
-                            onClick={() => props.history.push("/register")}
-                        >
-                            Sign up
-                        </Button>
+                            <MenuItem>{label}</MenuItem>
+                        </Link>
+                    ] : [
                     ]}
-            </section>
-        </div>
+                </React.Fragment>
+            );
+        });
+    };
+
+    const getMenuButtons = () => {
+        return headersData.map(({ label, req_login, req_logout, className, onClickFun }) => {
+            return (
+                <React.Fragment>
+                    { ((!!(user.user) && req_login) || !(user.user) && req_logout || req_logout === req_login) ? [
+                        <Button
+                            {...{
+                                key: label,
+                                onClick: onClickFun,
+                                className: className,
+                            }}
+                        >
+                            {label}
+                        </Button>
+                    ] : [
+                    ]}
+                </React.Fragment>
+            )
+        })
 
     };
 
     return (
         <header>
             <AppBar className={header} position="sticky">
-                {displayHeader()}
+                {mobileView ? displayMobile() : displayDesktop()}
             </AppBar>
         </header>
     );
