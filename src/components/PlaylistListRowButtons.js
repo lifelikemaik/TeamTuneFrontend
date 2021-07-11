8b0ec114d5e05cc4af40cbc5b33a514b28d48c1c
David Satomi <d.satomi@outlook.de>
2021-07-12 00:29:32 +0200

    Add Snackbar with fail safes and additional preventBackgroundClick function

--- src/components/PlaylistListRowButtons.js
+++ src/components/PlaylistListRowButtons.js
@@ -1,7 +1,8 @@
-import {Button, TableCell} from "@material-ui/core";
+import {Button, Snackbar, TableCell} from "@material-ui/core";
 import PropTypes from "prop-types";
 import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
+import {Alert} from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) => ({
     button: {
@@ -11,6 +12,33 @@ const useStyles = makeStyles((theme) => ({
 
 function PlaylistListRowButtons(props) {
     const {button} = useStyles();
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') return;
+        if(event != null) preventBackgroundClick(event)
+        setOpen(false);
+    };
+
+    const preventBackgroundClick = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+    }
+
+    const snackBar = () => {
+        return (
+            <Snackbar open={open} autoHideDuration={2500} onClose={handleClose} onClick={preventBackgroundClick}>
+                <Alert onClose={handleClose} severity="success">
+                    Share link copied to clipboard
+                </Alert>
+            </Snackbar>
+        )
+    }
 
     return (
         <div>
@@ -22,8 +50,7 @@ function PlaylistListRowButtons(props) {
                     ) : (
                         <div>
                             <Button className={button} variant="contained" onClick={(e) => {
-                                e.preventDefault()
-                                e.stopPropagation()
+                                preventBackgroundClick(e)
                                 props.onClickFollowPlaylist(props.playlist.public_id)
                             }}>
                                 Follow
@@ -38,9 +65,14 @@ function PlaylistListRowButtons(props) {
                 <div>
                     {props.playlist.is_own_playlist ? (
                         <div>
-                            <Button variant="contained">
+                            <Button variant="contained" onClick={(e) => {
+                                preventBackgroundClick(e)
+                                handleClick()
+                                navigator.clipboard.writeText("http://localhost:3000/invite/" + props.playlist._id)
+                            }}>
                                 Share Link
                             </Button>
+                            {snackBar()}
                             <Button variant="contained">
                                 Share Playlist
                             </Button>
